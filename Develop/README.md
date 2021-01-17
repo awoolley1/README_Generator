ede66554268373cfb9ad625b34bb195745fa5b88
Local Al <alexander.s.woolley@gmail.com>
2021-01-16 18:51:46 -0700

    badge rendering added

--- Develop/README.md
+++ Develop/README.md
@@ -1,33 +1,34 @@
 
-# **Title: gfgfg**
+# **Title: fsgfg**
 
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 #### **Table of Contents:**
-1. [*Description*](#description "Jump to Description")
-2. [*Installation*](#installation "Jump to Installation")
-3. [*Usage*](#usage "Jump to Usage")
-4. [*Contributing*](#contributing "Jump to Contributing")
-5. [*Tests*](#tests "Jump to Tests")
-6. [*Licenses*](#license "Jump to Licenses")
-7. [*Questions*](#questions "Jump to Questions")
+1. [*Description*](#description: "Jump to Description")
+2. [*Installation*](#installation: "Jump to Installation")
+3. [*Usage*](#usage: "Jump to Usage")
+4. [*Contributing*](#contributing: "Jump to Contributing")
+5. [*Tests*](#tests: "Jump to Tests")
+6. [*Licenses*](#license: "Jump to Licenses")
+7. [*Questions*](#questions: "Jump to Questions")
   
-### **Description**
-gdfg
+### **Description:**
+dgg
   
-### **Installation**
-fgfdg
+### **Installation:**
+dgdsgh
   
-### **Usage**
-fgdfg
+### **Usage:**
+fgdsgd
   
-### **Contributing**
-fgfg
+### **Contributing:**
+dsgfsdgfs
   
-### **Tests**
-fgfg
+### **Tests:**
+dgdg
   
-##### **License**
-testing
+### **License:**
+Apache
   
-##### **Questions**
-Contact me on GitHub @ fgfg or by Email at fgfg
+### **Questions:**
+Contact me on GitHub @ dgdg or by Email at dsgdsg
 
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,21 @@
+var licenseBadgeURL = ""
 // TODO: Create a function that returns a license badge based on which license is passed in
 function renderLicenseBadge(license) {
     switch (license) {
       case "MIT":
-        "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
+        licenseBadgeURL = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)"
         break;
       case "Apache":
-      "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
+        licenseBadgeURL = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
         break;
       case "BSD":
-        "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
+        licenseBadgeURL = "[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
         break;
       case "ISC":
-        "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+        licenseBadgeURL = "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
         break;
       case "GPL":
-        "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
+        licenseBadgeURL = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
         break;
       default:
           ""
@@ -24,41 +25,40 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(responses) {
+renderLicenseBadge(responses.License);
 
-  const licenseBadgeURL = renderLicenseBadge(responses.License);
   return `
 # **Title: ${responses.Title}**
 
 ${licenseBadgeURL}
-
 #### **Table of Contents:**
-1. [*Description*](#description "Jump to Description")
-2. [*Installation*](#installation "Jump to Installation")
-3. [*Usage*](#usage "Jump to Usage")
-4. [*Contributing*](#contributing "Jump to Contributing")
-5. [*Tests*](#tests "Jump to Tests")
-6. [*Licenses*](#license "Jump to Licenses")
-7. [*Questions*](#questions "Jump to Questions")
+1. [*Description*](#description: "Jump to Description")
+2. [*Installation*](#installation: "Jump to Installation")
+3. [*Usage*](#usage: "Jump to Usage")
+4. [*Contributing*](#contributing: "Jump to Contributing")
+5. [*Tests*](#tests: "Jump to Tests")
+6. [*Licenses*](#license: "Jump to Licenses")
+7. [*Questions*](#questions: "Jump to Questions")
   
-### **Description**
+### **Description:**
 ${responses.Description}
   
-### **Installation**
+### **Installation:**
 ${responses.Installation}
   
-### **Usage**
+### **Usage:**
 ${responses.Usage}
   
-### **Contributing**
+### **Contributing:**
 ${responses.Contributing}
   
-### **Tests**
+### **Tests:**
 ${responses.Tests}
   
-##### **License**
+### **License:**
 ${responses.License}
   
-##### **Questions**
+### **Questions:**
 Contact me on GitHub @ ${responses.GitHub} or by Email at ${responses.Email}
 
 `;
